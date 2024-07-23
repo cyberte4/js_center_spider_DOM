bd590efdf17710a1cee5497b84432e1d7092152c
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-07-23 18:56:00 +0300

    make build

---
+++ index.cffde1ac.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left=Math.round(t.clientWidth/2-e.offsetWidth/2)+"px",e.style.top=Math.round(t.clientHeight/2-e.offsetHeight/2)+"px";
+//# sourceMappingURL=index.cffde1ac.js.map
